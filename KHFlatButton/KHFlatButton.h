c794f8e1e47bd8de47c29ffdebd52bf2b58a0b64
Kyle Horn <kylehorn@me.com>
2014-07-15 22:11:44 -0500

    fixed versions

--- KHFlatButton/KHFlatButton.h
+++ KHFlatButton/KHFlatButton.h
@@ -9,13 +9,13 @@
 
 @interface KHFlatButton : UIButton
 
+@property (nonatomic) CGFloat cornerRadius;
+
 + (KHFlatButton *)buttonWithFrame:(CGRect)frame withTitle:(NSString *)title backgroundColor:(UIColor *)backgroundColor cornerRadius:(CGFloat)radius;
 
 // Factory method that returns a button with default corner radius of 3.0
 + (KHFlatButton *)buttonWithFrame:(CGRect)frame withTitle:(NSString *)title backgroundColor:(UIColor *)backgroundColor;
 
 + (UIBarButtonItem *)barButtonWithTitle:(NSString *)title backgroundColor:(UIColor *)backgroundColor;
-
-@property (nonatomic) CGFloat cornerRadius;
 
 @end
